deeaa75fee68ad6d44c26afdec6e2d1ff4e25dc1
matt330 <mwelc64@yahoo.com>
2024-12-24 10:28:14 -0500

    updated

--- src/app.js
+++ src/app.js
@@ -17,8 +17,8 @@ function mainMenu() {
                     'Add an employee',
                     'Update an employee role',
                     'Delete a department',
-                    'Delete a role', // New option
-                    'Delete an employee', // New option
+                    'Delete a role',
+                    'Delete an employee',
                     'Exit',
                 ],
             },
@@ -64,6 +64,85 @@ function mainMenu() {
 }
 
 // Other functions (viewDepartments, viewRoles, etc.) remain unchanged
+
+// Add an employee
+function addEmployee() {
+    pool.query('SELECT * FROM role', (err, res) => {
+        if (err) {
+            console.error(err);
+            mainMenu();
+            return;
+        }
+
+        const roles = res.rows.map((role) => ({
+            name: role.title,
+            value: role.id,
+        }));
+
+        pool.query('SELECT * FROM employee', (err, res) => {
+            if (err) {
+                console.error(err);
+                mainMenu();
+                return;
+            }
+
+            const managers = res.rows.map((employee) => ({
+                name: `${employee.first_name} ${employee.last_name}`,
+                value: employee.id,
+            }));
+            managers.unshift({ name: 'None', value: null });
+
+            inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'firstName',
+                        message: "Enter the employee's first name:",
+                    },
+                    {
+                        type: 'input',
+                        name: 'lastName',
+                        message: "Enter the employee's last name:",
+                    },
+                    {
+                        type: 'list',
+                        name: 'roleId',
+                        message: "Select the employee's role:",
+                        choices: roles,
+                    },
+                    {
+                        type: 'list',
+                        name: 'managerId',
+                        message: "Select the employee's manager:",
+                        choices: managers,
+                    },
+                ])
+                .then((answers) => {
+                    const query =
+                        'INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ($1, $2, $3, $4)';
+                    pool.query(
+                        query,
+                        [
+                            answers.firstName,
+                            answers.lastName,
+                            answers.roleId,
+                            answers.managerId,
+                        ],
+                        (err) => {
+                            if (err) {
+                                console.error(err);
+                            } else {
+                                console.log(
+                                    `Added employee: ${answers.firstName} ${answers.lastName}`
+                                );
+                            }
+                            mainMenu();
+                        }
+                    );
+                });
+        });
+    });
+}
 
 // Delete a role
 function deleteRole() {
